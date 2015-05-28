e521b7780d74bed74a43650ca5ca531b0ef3e077
hobnob11 <williamhobson62@aol.com>
2015-05-28 15:23:07 +0100

    players

--- players.php
+++ players.php
@@ -10,5 +10,25 @@
 
 <form action="index.php"><input value="Main Menu" type="submit"></form>
 
+<?php $player="Hobnob"; ?>
+
+
+<table style="width:100%">
+<tr>
+<th></th>
+<th><?php echo($player)?></th>
+<th></th>
+
+</tr>
+<tr>
+<td></td>
+<td>
+
+<img src="art/gui/equipment/eq_background.png"></img>
+
+</td>
+<td></td>
+</tr>
+</table>
 </body>
 </html>
