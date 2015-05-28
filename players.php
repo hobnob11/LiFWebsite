38bdb811c3928b1da4780c3f653b30d5618bd841
NinjrKillr <ninjrkillr@gmail.com>
2015-05-29 02:45:15 +1200

    merge some bullshit. let's hope all this works.

--- players.php
+++ players.php
@@ -1,36 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
+<?php include("header.html") ?>
 
-#equipment{
-	width:350px; 
-	height:579px;
-	background-image:url("art/gui/equipment/eq_background.png");
-	}
-#name{
-	position:relative;
-	width:100%;
-	height:16px;
-	text-align:center;
-	padding-top:20px;
-}
-		
-</style>
-
-</head>
-<body>
-
-<form action="index.php"><input value="Main Menu" type="submit"></form>
+<a href="index.php" class="btn btn-default">Index</a>
 
 <?php $player="Hobnob"; ?>
 
 <div id="equipment">
 	<div id="name"><?php echo($player);?></div>
 	
-	
-	
 </div>
 
-</body>
-</html>
+<?php include("footer.html") ?>
